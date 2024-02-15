f4f5ddd319acd816fb9a10b6d4e5baab10c13e03
Faizan Siddiqui <faizansiddiquirmr@gmail.com>
2024-02-15 14:35:53 +0530

    changed nodemon

--- QuizManager/src/app.ts
+++ QuizManager/src/app.ts
@@ -23,7 +23,7 @@ const connectionString = process.env.CONNECTION_STRING || "";
 
 app.use(express.json());
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://quiz-app-backend-seven.vercel.app/", credentials: true }));
 
 declare global {
   namespace Express {
